02ad21610b7316a870b5b77407aa007a07c9861d
beautycodez <angellorl_94@hotmail.com>
2024-03-24 14:12:12 -0500

    validation

---
+++ routes/accountRoute.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = new express.Router();
+const utilities = require("../utilities");
+const accountController = require("../controllers/accountController");
+const regValidate = require("../utilities/account-validation");
+
+// Route to build account login
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+// Route to build account sign in
+router.get(
+  "/register",
+  utilities.handleErrors(accountController.buildRegister)
+);
+
+// Router to send data to the database
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+);
+
+module.exports = router;
